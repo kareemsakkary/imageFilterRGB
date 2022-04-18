51c43a088d8b3b3c7264ba2e1d70eed2d2026907
Nada Mohammed <naddaa26@gmail.com>
2022-04-18 19:28:38 +0200

    Invert filter RGB

---
+++ cmake-build-debug/CMakeFiles/imageFilterRGB.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/imageFilterRGB.dir/main.cpp.obj"
+  "CMakeFiles/imageFilterRGB.dir/main.cpp.obj.d"
+  "imageFilterRGB.exe"
+  "imageFilterRGB.exe.manifest"
+  "imageFilterRGB.pdb"
+  "libimageFilterRGB.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/imageFilterRGB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
